15930c5942bc2c875bbfd405c37a0a3aadee4817
GilleM <matea.leka@gmail.com>
2021-06-27 21:41:14 +0000

    Last pushe before midnight

--- baby_corner/settings.py
+++ baby_corner/settings.py
@@ -106,12 +106,18 @@ WSGI_APPLICATION = 'baby_corner.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 #DATABASES = {
 #    'default': dj_database_url.parse('postgres://gdbdhkferjkhxs:1f58447f4ea17cb015809028839584f8a831bc5e000e4b38b4f8631ff1dda7c6@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/d50og55a91pu2t')
