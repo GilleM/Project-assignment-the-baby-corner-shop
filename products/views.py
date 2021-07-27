34c2df21737ba0699e35aaf5dbafcea1535b3aea
GilleM <matea.leka@gmail.com>
2021-07-27 19:21:05 +0000

    The JS for slider image added to index.html page

--- products/views.py
+++ products/views.py
@@ -5,7 +5,6 @@ from django.db.models.functions import Lower
 
 from .models import Product, Category
 
-# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
